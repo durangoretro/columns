8a3d6518593c19936f20395e558a34908f6f4714
Carlos Santisteban <zuiko21@gmail.com>
2024-09-05 23:03:28 +0200

    9 bars ready

--- music/clotho.s
+++ music/clotho.s
@@ -7,12 +7,14 @@
 ; bass (one octave higher than original)
 score1:
 	.byt	13, 0, $0,		13, 0, $0,	13, 0, $0,		13, 0, $0	; four bars of rest
+
 	.byt	13, 64, $2F,	8, 64, $2F,	13, 64, $2F,	16, 64, $2F	; A3 E3 A3 C4 crotchets
 	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; B3 G#3 E3 G#3 crotchets
 	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; G3 B3 D4 G3 crotchets
 	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; B3 D3 G3 B3 crotchets
 	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A3 E3 A3 B3 quavers
 	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; C4 B3 A3 E3 quavers
+	
 	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
 	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
 	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
@@ -41,6 +43,16 @@ score2:
 	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 D4 G4 D4 quavers
 	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; F#4 D4 G4 D4 quavers
 	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 D4 G4 D4 quavers
+	
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 E4 C5 E4 quavers
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; B4 E4 A4 E4 quavers
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; D5 E4 B4 E4 quavers
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; G#4 E4 B4 E4 quavers
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; G4 D4 B4 D4 quavers
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 D4 C5 D4 quavers
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; B4 D4 G4 D4 quavers
+	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; F#4 D4 G4 D4 quavers
+	.byt	, 192, $1F,	, 32, $3F,	, 32, $3F	; C5 dotted minim C5 D5 quavers
 
 	.byt	$FF				; repeat forever
 
