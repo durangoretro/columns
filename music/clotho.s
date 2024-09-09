1da98592616de96786e614fe4be4754ffb1c98b9
Carlos Santisteban <zuiko21@gmail.com>
2024-09-09 19:08:48 +0200

    upscale clotho

--- music/clotho.s
+++ music/clotho.s
@@ -2,57 +2,58 @@
 ; (c) 2024 Carlos J. Santisteban
 
 ; originally 120bpm, actually played at 117.1875 for better note accuracy
-; set sr_tempo to 1, sg_envsp to 16
+; since shortest note is quaver, and dotted is at least crotchet, could scale down 1:32
+; set sr_tempo to 31, sg_envsp to 16
 
-; bass (one octave higher than original)
+; bass (all one octave higher than original)
 score1:
-	.byt	13, 0, $0,		13, 0, $0,	13, 0, $0,		13, 0, $0	; four bars of rest
+	.byt	13, 32, $0		; four bars of rest
 
-	.byt	13, 64, $2F,	8, 64, $2F,	13, 64, $2F,	16, 64, $2F	; A3 E3 A3 C4 crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; B3 G#3 E3 G#3 crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; G3 B3 D4 G3 crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; B3 D3 G3 B3 crotchets
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A3 E3 A3 B3 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; C4 B3 A3 E3 quavers
+	.byt	13, 2, $2F,	8, 2, $2F,	13, 2, $2F,	16, 2, $2F	; A3 E3 A3 C4 crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; B3 G#3 E3 G#3 crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; G3 B3 D4 G3 crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; B3 D3 G3 B3 crotchets
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; A3 E3 A3 B3 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; C4 B3 A3 E3 quavers
 	
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 64, $2F,	, 64, $2F,	, 64, $2F,	, 64, $2F	; crotchets
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; quavers
-	.byt	, 128, $1F,	, 128, $1F	; minims
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 2, $2F,	, 2, $2F,	, 2, $2F,	, 2, $2F	; crotchets
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; quavers
+	.byt	, 4, $1F,	, 4, $1F	; minims
 	.byt	$FF				; repeat forever
 
 ; middle voice
 score2:
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 E4 C5 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; B4 E4 A4 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; G#4 E4 B4 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 E4 G#4 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; G4 D4 B4 D4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 D4 G4 D4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; F#4 D4 G4 D4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 D4 G4 D4 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; A5 E5 C6 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; B5 E5 A5 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; G#5 E5 B5 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; A5 E5 G#5 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; G5 D5 B5 D5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; A5 D5 G5 D5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; F#5 D5 G5 D5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; A5 D5 G5 D5 quavers
 	
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 E4 C5 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; B4 E4 A4 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; D5 E4 B4 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; G#4 E4 B4 E4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; G4 D4 B4 D4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; A4 D4 C5 D4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; B4 D4 G4 D4 quavers
-	.byt	, 32, $3F,	, 32, $3F,	, 32, $3F,	, 32, $3F	; F#4 D4 G4 D4 quavers
-	.byt	, 192, $1F,	, 32, $3F,	, 32, $3F	; C5 dotted minim C5 D5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; A5 E5 C6 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; B5 E5 A5 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; D6 E5 B5 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; G#5 E5 B5 E5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; G5 D5 B5 D5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; A5 D5 C6 D5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; B5 D5 G5 D5 quavers
+	.byt	, 1, $3F,	, 1, $3F,	, 1, $3F,	, 1, $3F	; F#5 D5 G5 D5 quavers
+	.byt	, 6, $1F,	, 1, $3F,	, 1, $3F	; C6 dotted minim C6 D6 quavers
 
 	.byt	$FF				; repeat forever
 
